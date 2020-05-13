f7893f4300bf357a4372b83642b42d10786a2dbd
jebyrnes <jarrett.byrnes@umb.edu>
2020-05-12 20:48:29 -0400

    Add get_info

---
+++ R/get_info_covid19mobility.R
@@ -0,0 +1,45 @@
+#' Get information about the datasets provided by covid19mobility
+#'
+#' @description Returns information about the datasets in this package for covid19R harvesting
+#'
+#' @return a tibble of information about the datasets in this package
+#' @export get_info_covid19mobility
+#'
+#' @examples
+#' \dontrun{
+#'
+#' # get the dataset info from this package
+#' get_info_covid19mobility()
+#' }
+#'
+get_info_covid19mobility <- function(){
+  dplyr::tribble(
+    ~data_set_name, ~package_name, ~function_to_get_data,
+    ~data_details, ~data_url, ~license_url,
+    ~data_types, ~location_types,
+    ~spatial_extent, ~has_geospatial_info,
+
+    "covid19mobility_country",
+    "covid19mobility",
+    "refresh_covid19mobility_country",
+    "Data reflects relative volume of directions requests compared to a baseline volume on January 13th, 2020 for multiple transportation modes aggregated at the country level.",
+    "https://www.apple.com/covid19/mobility",
+    "https://www.apple.com/covid19/mobility",
+    "driving, walking, transit", #COMMA SEPARATED STRING OF DATA TYPES
+    "country", #COMMA SEPARATED STRING OF LOCATION TYPES
+    "global", #HOW LARGE IS THE AREA COVERED BY THE WHOLE DATASET? COUNTRY? CONTINENT? WORLD? OTHER?
+    FALSE, #IS THERE GEOSPATIAL INFORMATION, E.G. LAT/LONG? TRUE/FALSE
+
+    "covid19mobility_subregion",
+    "covid19mobility",
+    "refresh_covid19mobility_subregion",
+    "Data reflects relative volume of directions requests compared to a baseline volume on January 13th, 2020 for multiple transportation modes aggregated at the subregion (state) level.",
+    "https://www.apple.com/covid19/mobility",
+    "https://www.apple.com/covid19/mobility",
+    "driving, walking, transit", #COMMA SEPARATED STRING OF DATA TYPES
+    "state", #COMMA SEPARATED STRING OF LOCATION TYPES
+    "global", #HOW LARGE IS THE AREA COVERED BY THE WHOLE DATASET? COUNTRY? CONTINENT? WORLD? OTHER?
+    FALSE #IS THERE GEOSPATIAL INFORMATION, E.G. LAT/LONG? TRUE/FALSE
+  )
+
+}
