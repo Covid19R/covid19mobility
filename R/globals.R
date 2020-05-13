34496598d0b7b284922b5df7d1fc4e3fdae79e05
jebyrnes <jarrett.byrnes@umb.edu>
2020-05-12 20:48:55 -0400

    Update globals for new functions

--- R/globals.R
+++ R/globals.R
@@ -9,6 +9,29 @@ globalVariables(
     "location_code_type",
     "data_type",
     "value",
+    "data_set_name",
+    "package_name",
+    "function_to_get_data",
+    "data_details",
+    "data_url",
+    "license_url",
+    "data_types",
+    "has_geospatial_info",
+
+
+    #from apple or geospatial sources
+    "geo_type",
+    "transportation_type",
+    "ISO_3166_2",
+    "name_en",
+    "name",
+    "subdivision",
+    "un_locode",
+    "alternative_name",
+    "region",
+    "NAME.EN",
+    "old_loc",
+
 
     #some internal functions
     "add_city_codes",
