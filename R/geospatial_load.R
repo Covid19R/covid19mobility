a31d7d249e93bdb2d81329590e02a6ff6a752d4a
jebyrnes <jarrett.byrnes@umb.edu>
2020-05-16 14:54:20 -0400

    Refactor locode loading

---
+++ R/geospatial_load.R
@@ -0,0 +1,35 @@
+load_un_locode <- function(){
+  # get city codes
+  locode_url <- "https://github.com/jebyrnes/unlocodeR/raw/master/data/unlocode.rda"
+  f <- tempfile()
+  utils::download.file(locode_url, f)
+  load(f) # loads unlocode
+  unlink(f)
+
+  # some changes to the locode to make merging happen
+  unlocode <- unlocode %>%
+    dplyr::mutate(
+      name_en = ifelse(name_en == "New York" & subdivision == "NY", "New York City", name_en),
+      name_en = ifelse(name_en == "Washington" & subdivision == "DC", "Washington DC", name_en),
+      name_en = ifelse(un_locode == "US STL", "St. Louis", name_en),
+      name_en = ifelse(un_locode == "DE FRA", "Frankfurt", name_en),
+      name_en = ifelse(un_locode == "IN BOM", "Mumbai", name_en),
+      name_en = ifelse(un_locode == "GB BHM", "Birmingham - UK", name_en),
+      name_en = ifelse(un_locode == "US BHM", "Birmingham - Alabama", name_en),
+      name_en = ifelse(un_locode == "DE BOM", "Bochum - Dortmund", name_en),
+      name_en = ifelse(name_en == "San Francisco" & subdivision == "CA", "San Francisco - Bay Area", name_en),
+      name_en = ifelse(name_en == "Ciudad de Mexico", "Mexico City", name_en),
+      name_en = ifelse(name_en == "Nagoya, Aichi", "Nagoya", name_en),
+      name_en = ifelse(name_en == "Helsingfors (Helsinki)", "Helsinki", name_en),
+      name_en = ifelse(name_en == "Holon/Tel Aviv", "Tel Aviv", name_en),
+      name_en = ifelse(name_en == "Jakarta, Java", "Jakarta", name_en),
+      name_en = ifelse(name_en == "Saint Petersburg", "Saint Petersburg - Clearwater (Florida)", name_en),
+      name_en = ifelse(name_en == "Saint Petersburg (ex Leningrad)", "Saint Petersburg - Russia", name_en),
+      name_en = ifelse(name_en == "Brussel (Bruxelles)", "Brussels", name_en),
+      name_en = ifelse(name_en == "Vienna", "Vienna_", name_en),
+      name_en = ifelse(un_locode == "AT VIE", "Vienna", name_en),
+      name_en = ifelse(name_en == "Bridgeport", "Bridgeport_", name_en),
+      name_en = ifelse(un_locode == "US BDR", "Bridgeport", name_en),
+    )
+
+}
--- R/geospatial_utils.R
+++ R/geospatial_utils.R
@@ -97,38 +97,7 @@ add_subregion_codes <- function(adf) {
 #' @return tibble
 #'
 add_city_codes <- function(adf) {
-  # get city codes
-  locode_url <- "https://github.com/jebyrnes/unlocodeR/raw/master/data/unlocode.rda"
-  f <- tempfile()
-  utils::download.file(locode_url, f)
-  load(f) # loads unlocode
-  unlink(f)
-
-  # some changes to the locode to make merging happen
-  unlocode <- unlocode %>%
-    dplyr::mutate(
-      name_en = ifelse(name_en == "New York" & subdivision == "NY", "New York City", name_en),
-      name_en = ifelse(name_en == "Washington" & subdivision == "DC", "Washington DC", name_en),
-      name_en = ifelse(un_locode == "US STL", "St. Louis", name_en),
-      name_en = ifelse(un_locode == "DE FRA", "Frankfurt", name_en),
-      name_en = ifelse(un_locode == "IN BOM", "Mumbai", name_en),
-      name_en = ifelse(un_locode == "GB BHM", "Birmingham - UK", name_en),
-      name_en = ifelse(un_locode == "US BHM", "Birmingham - Alabama", name_en),
-      name_en = ifelse(un_locode == "DE BOM", "Bochum - Dortmund", name_en),
-      name_en = ifelse(name_en == "San Francisco" & subdivision == "CA", "San Francisco - Bay Area", name_en),
-      name_en = ifelse(name_en == "Ciudad de Mexico", "Mexico City", name_en),
-      name_en = ifelse(name_en == "Nagoya, Aichi", "Nagoya", name_en),
-      name_en = ifelse(name_en == "Helsingfors (Helsinki)", "Helsinki", name_en),
-      name_en = ifelse(name_en == "Holon/Tel Aviv", "Tel Aviv", name_en),
-      name_en = ifelse(name_en == "Jakarta, Java", "Jakarta", name_en),
-      name_en = ifelse(name_en == "Saint Petersburg", "Saint Petersburg - Clearwater (Florida)", name_en),
-      name_en = ifelse(name_en == "Saint Petersburg (ex Leningrad)", "Saint Petersburg - Russia", name_en),
-      name_en = ifelse(name_en == "Brussel (Bruxelles)", "Brussels", name_en),
-      name_en = ifelse(name_en == "Vienna", "Vienna_", name_en),
-      name_en = ifelse(un_locode == "AT VIE", "Vienna", name_en),
-      name_en = ifelse(name_en == "Bridgeport", "Bridgeport_", name_en),
-      name_en = ifelse(un_locode == "US BDR", "Bridgeport", name_en),
-    )
+ unlocode <- load_un_locode()
 
   # merge in codes and pivot to covid19R standard and return
   adf2 <- location_code_merge_pivot(unlocode, adf,
@@ -136,21 +105,6 @@ add_city_codes <- function(adf) {
     code_col = "un_locode"
   )
 
-  # check for dups
-  # dups <- adf2 %>%
-  #   dplyr::group_by(location, date, data_type) %>%
-  #   dplyr::count() %>%
-  #   dplyr::ungroup() %>%
-  #   dplyr::filter(n>1) %>%
-  #   dplyr::group_by(location) %>%
-  #   dplyr::slice(1L)
-  #
-  # View(dups)
-  #
-  # unlocode %>% dplyr::filter(grepl("Bridgeport", name_en)) %>% View
-  # unlocode %>% dplyr::filter(grepl("GR ATH", un_locode)) %>% View
-
-  # return
   adf
 }
 
